4843f2f0b7c3ca610027b6f3f11966fc3ab7ca7d
apierc2175 <aidanwpierce@gmail.com>
2017-05-11 16:12:22 -0400

    Add files via upload

---
+++ js/noah.js
@@ -0,0 +1,13 @@
+
+function noahGay() {
+  var message;
+  var dog = true;
+
+  if(dog = true){
+    message = "You Will Have Sex With Will Tomorrow";
+  } else {
+    console.log("gay");
+  }
+  document.getElementById('noah').innerHTML = message;
+
+  }
